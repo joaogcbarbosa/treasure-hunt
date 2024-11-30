237c5960b90efa68fb839eadc28878862d4dc053
joaogcbarbosa <joaogcb29@gmail.com>
2024-11-30 20:44:56 -0300

    Refactor: move to utils

--- debug_client.py
+++ debug_client.py
@@ -1,17 +1,10 @@
 from treasure_hunt.client.client import client, send_message
-from time import sleep
-
-def options_template():
-    print("#"*20)
-    print("Press [S] to send message.")
-    print("Press [Q] to quit.")
-    print("#"*20)
-
+from treasure_hunt.utils import options
 
 if __name__ == "__main__":
     with client() as c:
         while True:
-            options_template()
+            options()
             choice = input("Choose an option: ")
             if choice == "S":
                 msg = input("Message: ")
