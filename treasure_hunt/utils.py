9d6ba1936859c838f602f786d3a2cc373e94fc00
joaogcbarbosa <joaogcb29@gmail.com>
2024-11-30 20:45:17 -0300

    Feat: template module

---
+++ treasure_hunt/utils.py
@@ -0,0 +1,13 @@
+def print_treasure_hunt_title():
+    print("\n" + "=" * 40)
+    print("        🌟 TREASURE-HUNT 🌟")
+    print("=" * 40)
+    print("🪙 Prepare-se para a aventura! 🏴‍☠️\n")
+
+def options():
+    print("#"*20)
+    print("Press [S] to send message.")
+    print("Press [Q] to quit.")
+    print("#"*20)
+
+print_treasure_hunt_title()
