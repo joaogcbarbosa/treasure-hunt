026b62fca28079c0cc0ef117166a38e76be9fd62
joaogcbarbosa <joaogcb29@gmail.com>
2024-11-30 19:00:53 -0300

    Refactor: options template

--- debug_client.py
+++ debug_client.py
@@ -1,8 +1,20 @@
 from treasure_hunt.client.client import client, send_message
 from time import sleep
 
+def options_template():
+    print("#"*20)
+    print("Press [S] to send message.")
+    print("Press [Q] to quit.")
+    print("#"*20)
+
+
 if __name__ == "__main__":
     with client() as c:
-        send_message("Oi", c)
-        sleep(5)
-        send_message("Oi de novo", c)
+        while True:
+            options_template()
+            choice = input("Choose an option: ")
+            if choice == "S":
+                msg = input("Message: ")
+                send_message(msg, c)
+            else:
+                break
