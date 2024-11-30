146f0f467ef4f8c3ad80145a85610379b3772178
joaogcbarbosa <joaogcb29@gmail.com>
2024-11-30 13:32:32 -0300

    Feat: server exemple

---
+++ treasure_hunt/server/server_eg.py
@@ -0,0 +1,19 @@
+from socket import socket, AF_INET, SOCK_STREAM
+from ..constants import MAX_PLAYERS, HOST, PORT
+
+server_socket = socket(family=AF_INET, type=SOCK_STREAM)
+server_socket.bind((HOST, PORT))
+server_socket.listen(MAX_PLAYERS)
+
+print("Listening!")
+conn, addr = server_socket.accept()
+print(f"Connected by {addr}")
+
+while True:
+    msg = conn.recv(1024)
+    if not msg:
+        break
+    decoded_msg = msg.decode("utf-8")
+    print(decoded_msg)
+
+conn.close()
