9616cf476fd89423b28f0a82c6488d6f7bf87510
joaogcbarbosa <joaogcb29@gmail.com>
2024-11-30 21:51:37 -0300

    Refator: run_server inside main

--- treasure_hunt/server/server.py
+++ treasure_hunt/server/server.py
@@ -1,19 +1,26 @@
 from socket import socket, AF_INET, SOCK_STREAM
 from ..utils.constants import MAX_PLAYERS, HOST, PORT
 
-server_socket = socket(family=AF_INET, type=SOCK_STREAM)
-server_socket.bind((HOST, PORT))
-server_socket.listen(MAX_PLAYERS)
+def run_server():
+    server_socket = socket(family=AF_INET, type=SOCK_STREAM)
+    server_socket.bind((HOST, PORT))
+    server_socket.listen(MAX_PLAYERS)
 
-print("Listening!")
-conn, addr = server_socket.accept()
-print(f"Connected by {addr}")
+    conn, addr = server_socket.accept()
+    print(f"Server initialized on {addr}")
 
-while True:
-    msg = conn.recv(1024)
-    if not msg:
-        break
-    decoded_msg = msg.decode("utf-8")
-    print(decoded_msg)
+    while True:
+        msg = conn.recv(1024)
+        if not msg:
+            break
+        decoded_msg = msg.decode("utf-8")
+        print(decoded_msg)
 
-conn.close()
+    conn.close()
+
+
+def main():
+    run_server()
+
+
+main()
