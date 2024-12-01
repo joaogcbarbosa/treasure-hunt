96ea789e7c3f989d8f7a9e2cc0563d9c72553cc6
joaogcbarbosa <joaogcb29@gmail.com>
2024-12-01 00:18:50 -0300

    Refactor: passing port and host to client context manager

--- debug_client.py
+++ debug_client.py
@@ -1,7 +1,8 @@
 from treasure_hunt.client.client import client
+from treasure_hunt.utils.constants import SERVER_PORT, SERVER_HOST
 
 if __name__ == "__main__":
-    with client() as c:
+    with client(SERVER_HOST, SERVER_PORT) as c:
         while True:
             data = c.recv(1024)
             if not data:  # Se data for vazio, a conexão foi fechada
--- treasure_hunt/client/client.py
+++ treasure_hunt/client/client.py
@@ -1,14 +1,13 @@
 from socket import socket, AF_INET, SOCK_STREAM
 from typing import Any, Generator
-from ..utils.constants import SERVER_PORT, SERVER_HOST
 from time import sleep
 from contextlib import contextmanager
 
 @contextmanager
-def client() -> Generator[socket, Any, Any]:
+def client(host: str, port: int) -> Generator[socket, Any, Any]:
     try:
         client_socket = socket(family=AF_INET, type=SOCK_STREAM)
-        client_socket.connect((SERVER_HOST, SERVER_PORT))
+        client_socket.connect((host, port))
         yield client_socket
     except Exception as e:
         print(f"Could not connect: {e}")
