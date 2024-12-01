8d58b6f968fafd4e9e35a1800e67d378b9df271f
joaogcbarbosa <joaogcb29@gmail.com>
2024-11-30 22:39:46 -0300

    Chore: debugging

--- debug_client.py
+++ debug_client.py
@@ -3,4 +3,7 @@ from treasure_hunt.client.client import client
 if __name__ == "__main__":
     with client() as c:
         while True:
-            pass  # just checking connection
+            data = c.recv(1024)
+            if not data:  # Se data for vazio, a conexão foi fechada
+                print("Server has reached its limit of connections.")
+                break
