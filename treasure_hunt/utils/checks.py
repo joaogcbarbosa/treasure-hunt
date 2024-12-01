4533a04b615ce6613877c28eb9280a1d43b804c5
joaogcbarbosa <joaogcb29@gmail.com>
2024-11-30 21:32:19 -0300

    Refactor: adds number of players's template

--- treasure_hunt/utils/checks.py
+++ treasure_hunt/utils/checks.py
@@ -1,10 +1,13 @@
 from .constants import MAX_PLAYERS, MIN_PLAYERS
-from .templates import number_of_players_warn
+from .templates import number_of_players_warn, number_of_players
+from time import sleep
 
 
 def check_number_of_players() -> int:
     nro_players = int(input().strip())
     while nro_players > MAX_PLAYERS or nro_players < MIN_PLAYERS:
         number_of_players_warn()
+        sleep(1.5)
+        number_of_players()
         nro_players = int(input().strip())
     return nro_players
