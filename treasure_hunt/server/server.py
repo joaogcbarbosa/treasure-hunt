3f6703da912ddbd0d4fc34f27f431ae48449ec82
joaogcbarbosa <joaogcb29@gmail.com>
2024-11-30 22:17:53 -0300

    Refactor: using context manager

--- treasure_hunt/server/server.py
+++ treasure_hunt/server/server.py
@@ -1,19 +1,18 @@
 from socket import socket, AF_INET, SOCK_STREAM
 from ..utils.constants import HOST, PORT
 
-def run_server(number_of_players: int):
-    server_socket = socket(family=AF_INET, type=SOCK_STREAM)
-    server_socket.bind((HOST, PORT))
-    server_socket.listen(number_of_players)
+def run_server(number_of_players: int) -> None:
+    with socket(AF_INET, SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
 
-    conn, addr = server_socket.accept()
-    print(f"Server initialized on {addr}")
-
-    while True:
-        msg = conn.recv(1024)
-        if not msg:
-            break
-        decoded_msg = msg.decode("utf-8")
-        print(decoded_msg)
-
-    conn.close()
+        accepted_connections = 0
+        print("Running server...")
+        while True:
+            conn, addr = s.accept()
+            if accepted_connections < number_of_players:
+                print(f"Connection from {addr} accepted")
+                accepted_connections += 1
+            else:
+                print(f"Connection from {addr} refused")
+                conn.close()
