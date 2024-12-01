44c69d434f58d86bb1d53b6d34241c149a2be32d
joaogcbarbosa <joaogcb29@gmail.com>
2024-12-01 01:21:11 -0300

    Feat: spot_players

--- treasure_hunt/server/server.py
+++ treasure_hunt/server/server.py
@@ -1,12 +1,25 @@
 from socket import socket, AF_INET, SOCK_STREAM
 from ..utils.constants import HOST, PORT
 from ..models.map import GameMap
+from random import randint
 
 map = GameMap()
 
-def game():
-    pass
+def spot_players(number_of_players: int, map: GameMap) -> GameMap:
+    occupied_spots = []
+    for i in range(1, number_of_players + 1):
+        height_bound, width_bound = map.bounds
+        height, width = randint(0, height_bound), randint(0, width_bound)
+        while (height, width) in occupied_spots:
+            height, width = randint(0, height_bound), randint(0, width_bound)
+        map.update(height, width, f"P{i}")
+        occupied_spots.append((height, width))
+    return map
 
+
+def game(number_of_players: int):
+    map = spot_players(number_of_players, map)
+    map.display()
 
 
 def run_server(number_of_players: int) -> None:
@@ -22,7 +35,7 @@ def run_server(number_of_players: int) -> None:
                 print(f"Connection from {addr} accepted")
                 accepted_connections += 1
                 if accepted_connections == number_of_players:
-                    game()
+                    game(number_of_players)
                 print("Waiting for other players")
             else:
                 print(f"Connection from {addr} refused")
