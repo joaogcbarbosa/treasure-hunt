aa8475a5665e50852466b17a4d4365e724652a06
joaogcbarbosa <joaogcb29@gmail.com>
2024-12-01 00:47:08 -0300

    Feat: start_game func

--- treasure_hunt/server/server.py
+++ treasure_hunt/server/server.py
@@ -1,6 +1,10 @@
 from socket import socket, AF_INET, SOCK_STREAM
 from ..utils.constants import HOST, PORT
 
+def start_game():
+    pass
+
+
 def run_server(number_of_players: int) -> None:
     with socket(AF_INET, SOCK_STREAM) as s:
         s.bind((HOST, PORT))
@@ -13,6 +17,9 @@ def run_server(number_of_players: int) -> None:
             if accepted_connections < number_of_players:
                 print(f"Connection from {addr} accepted")
                 accepted_connections += 1
+                if accepted_connections == number_of_players:
+                    start_game()
+                print("Waiting for other players")
             else:
                 print(f"Connection from {addr} refused")
                 conn.close()
