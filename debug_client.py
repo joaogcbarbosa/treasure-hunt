2e192e975bfdc501628496c0029cb0d9fac59dc4
joaogcbarbosa <joaogcb29@gmail.com>
2024-11-30 21:00:51 -0300

    Fix: importation

--- debug_client.py
+++ debug_client.py
@@ -1,10 +1,10 @@
 from treasure_hunt.client.client import client, send_message
-from treasure_hunt.utils import options
+from treasure_hunt.utils.templates import move_options
 
 if __name__ == "__main__":
     with client() as c:
         while True:
-            options()
+            move_options()
             choice = input("Choose an option: ")
             if choice == "S":
                 msg = input("Message: ")
