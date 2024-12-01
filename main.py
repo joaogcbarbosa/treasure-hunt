de9031c1849def8c57dab6885577b2a8a6890eae
joaogcbarbosa <joaogcb29@gmail.com>
2024-11-30 21:28:39 -0300

    Refactor: small tweak

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from treasure_hunt.utils.templates import treasure_hunt_title, number_of_players
 from multiprocessing.pool import ThreadPool
+from treasure_hunt.utils.checks import check_number_of_players
 
 
 if __name__ == "__main__":
     treasure_hunt_title()
     number_of_players()
-    nro_players = int(input().strip())
-    with ThreadPool(nro_players) as p:
-        pass
+    nro_players = check_number_of_players()
+
