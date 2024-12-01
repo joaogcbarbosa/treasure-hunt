703f17dcc9d4a923d99151a495690bb17042148b
joaogcbarbosa <joaogcb29@gmail.com>
2024-11-30 22:17:39 -0300

    Refactor: running server

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from treasure_hunt.utils.templates import treasure_hunt_title, number_of_players
-from multiprocessing.pool import ThreadPool
 from treasure_hunt.utils.checks import check_number_of_players
+from treasure_hunt.server.server import run_server
 
 
 if __name__ == "__main__":
     treasure_hunt_title()
     number_of_players()
     nro_players = check_number_of_players()
-    # starting server..
+    run_server(nro_players)
     # connecting players...
     # starting game...
