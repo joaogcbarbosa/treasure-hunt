5b452a6504b56a871871c3a70cb4a3f331e6cdf5
joaogcbarbosa <joaogcb29@gmail.com>
2024-11-30 22:17:26 -0300

    Chore: testing conn

--- debug_client.py
+++ debug_client.py
@@ -1,13 +1,6 @@
-from treasure_hunt.client.client import client, send_message
-from treasure_hunt.utils.templates import move_options
+from treasure_hunt.client.client import client
 
 if __name__ == "__main__":
     with client() as c:
         while True:
-            move_options()
-            choice = input("Choose an option: ")
-            if choice == "S":
-                msg = input("Message: ")
-                send_message(msg, c)
-            else:
-                break
+            pass  # just checking connection
