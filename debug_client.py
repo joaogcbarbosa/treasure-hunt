e7bbb1998c551037216acfbedbc19708a2acffc2
joaogcbarbosa <joaogcb29@gmail.com>
2024-12-03 23:05:22 -0300

    Refactor: receiving json from server

--- debug_client.py
+++ debug_client.py
@@ -1,12 +1,15 @@
 from treasure_hunt.client.client import client
 from treasure_hunt.utils.constants import SERVER_PORT, SERVER_HOST
+import json
 
 if __name__ == "__main__":
     with client(SERVER_HOST, SERVER_PORT) as c:
         while True:
             data = c.recv(1024)
-            if not data:  # Se data for vazio, a conexão foi fechada
+            if not data:
                 print("Server has reached its limit of connections.")
                 break
-            mensagem = c.recv(1024).decode('utf-8')
-            print(mensagem)
+            data = c.recv(1024).decode("utf-8")
+            data = json.loads(data)
+            print(data["map_situation"])
+            choose = input(data["your_turn_sentence"])
