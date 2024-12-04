0c6d80723864031027eda3d788310a52d7162ede
joaogcbarbosa <joaogcb29@gmail.com>
2024-12-03 23:04:51 -0300

    Chore: adds comment

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from treasure_hunt.server.server import run_server
 
 
 if __name__ == "__main__":
+    # Terminal Template
     treasure_hunt_title()
     number_of_players()
+    # =================
     nro_players = check_number_of_players()
     run_server(nro_players)
     # connecting players...
