0c4bed7e776f84a752e8920b539b21b9c5fca80d
joaogcbarbosa <joaogcb29@gmail.com>
2024-12-04 23:10:04 -0300

    Refactor: debugging client

--- debug_client.py
+++ debug_client.py
@@ -1,18 +1,28 @@
 from treasure_hunt.client.client import client
 from treasure_hunt.utils.constants import SERVER_PORT, SERVER_HOST
-import json
+from treasure_hunt.utils.checks import check_possible_moves
+from treasure_hunt.utils.move import move_player
+from treasure_hunt.server.game_map import game_map
+from treasure_hunt.utils.converters import string_to_matrix
+import pickle
 
 if __name__ == "__main__":
     with client(SERVER_HOST, SERVER_PORT) as c:
         while True:
+
             data = c.recv(1024)
             if not data:
                 print("Server has reached its limit of connections.")
                 break
-            data = c.recv(1024).decode("utf-8")
-            data = json.loads(data)
-            map_situation = data["map_situation"]
+
+            data = c.recv(1024)
+            data = pickle.loads(data)
+            map_situation = string_to_matrix(data["map_situation"].display())
             your_turn_sentence = data["your_turn_sentence"]
+            player = data["player"]
+
             print(map_situation)
-            possible_moves = check_possible_moves(map_situation)
-            choose = input(your_turn_sentence)
+            possible_moves, player_pos = check_possible_moves(player, map_situation)
+            choice = input(your_turn_sentence)
+            move_player(choice, possible_moves, game_map, player_pos)
+            print(game_map.display())
