e13e09835aa56ab9e54f4f0d075c401456322b6d
joaogcbarbosa <joaogcb29@gmail.com>
2024-12-04 21:39:00 -0300

    Fix: parameter passage

--- treasure_hunt/utils/checks.py
+++ treasure_hunt/utils/checks.py
@@ -1,22 +1,22 @@
 from typing import Union
-from .constants import MAX_PLAYERS, MIN_PLAYERS
-from .templates import number_of_players_warn, number_of_players
-from time import sleep
-from ..server.game_map import game_map
-from itertools import product
+# from .constants import MAX_PLAYERS, MIN_PLAYERS
+# from .templates import number_of_players_warn, number_of_players
+# from time import sleep
+# from ..server.game_map import game_map
 
 
-def check_number_of_players() -> int:
-    nro_players = int(input().strip())
-    while nro_players > MAX_PLAYERS or nro_players < MIN_PLAYERS:
-        number_of_players_warn()
-        sleep(1.5)
-        number_of_players()
-        nro_players = int(input().strip())
-    return nro_players
+
+# def check_number_of_players() -> int:
+#     nro_players = int(input().strip())
+#     while nro_players > MAX_PLAYERS or nro_players < MIN_PLAYERS:
+#         number_of_players_warn()
+#         sleep(1.5)
+#         number_of_players()
+#         nro_players = int(input().strip())
+#     return nro_players
 
 
-def check_available_positions(positions: list[tuple[int]], game_map: list[list[Union[int, str]]]) -> bool:
+def check_available_positions(positions: list[tuple[int]], game_map: list[list[Union[int, str]]]) -> list[tuple[int]]:
     map_positions = []
     for i in range(len(game_map)):
         for j in range(len(game_map)):
@@ -25,6 +25,8 @@ def check_available_positions(positions: list[tuple[int]], game_map: list[list[U
     for p in positions:
         if p not in map_positions or isinstance(game_map[p[0]][p[1]], str):
             positions.remove(p)
+
+    return positions
 
 
 def check_player_position(game_map: list[list[Union[int, str]]], player: str) -> tuple[int, int]:
@@ -40,17 +42,17 @@ def check_player_position(game_map: list[list[Union[int, str]]], player: str) ->
     return x, y
 
 
-def check_possible_moves(player: str, game_map: list[list[Union[int, str]]]) -> list[str]:
+def check_possible_moves(player: str, game_map: list[list[Union[int, str]]]) -> list[tuple[int]]:
     player_pos = check_player_position(game_map, player)
     x, y = player_pos
     surrounding_positions = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
-    available_positions = check_available_positions(surrounding_positions)
-    return surrounding_positions
+    available_positions = check_available_positions(surrounding_positions, game_map)
+    return available_positions
 
 
 if __name__ == "__main__":
     map_situation = [
-        [2, 2, 2],
+        [2, 2, "P2"],
         [3, 3, "P1"],
         [4, 5, 6],
     ]
