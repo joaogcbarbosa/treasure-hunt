19c6cf416a67a6a1ed344f9ad086f132a0ce04fc
joaogcbarbosa <joaogcb29@gmail.com>
2024-12-08 12:38:18 -0300

    Refactor: removes not used importations

--- run_server.py
+++ run_server.py
@@ -1,16 +1,7 @@
-from multiprocessing.pool import ThreadPool
 from threading import Thread
 from treasure_hunt.utils.templates import treasure_hunt_title, number_of_players
-from treasure_hunt.utils.checks import check_number_of_players, check_possible_moves
+from treasure_hunt.utils.checks import check_number_of_players
 from treasure_hunt.server.server import server_runner
-from treasure_hunt.client.client import client
-from treasure_hunt.utils.constants import SERVER_HOST, SERVER_PORT
-from treasure_hunt.server.game_map import game_map
-from treasure_hunt.utils.move import move_player
-from treasure_hunt.utils.converters import string_to_matrix
-
-import pickle
-
 
 def main():
     # Terminal Template
