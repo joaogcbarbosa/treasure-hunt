ea5c74870d1338b124d134dd55d90cb7e352e573
joaogcbarbosa <joaogcb29@gmail.com>
2024-12-08 12:36:57 -0300

    Refactor: removes client runner

--- run_server.py
+++ run_server.py
@@ -11,27 +11,6 @@ from treasure_hunt.utils.converters import string_to_matrix
 
 import pickle
 
-def client_runner():
-    with client(SERVER_HOST, SERVER_PORT) as c:
-        while True:
-
-            data = c.recv(1024)
-            if not data:
-                print("Server has reached its limit of connections.")
-                break
-
-            data = c.recv(1024)
-            data = pickle.loads(data)
-            map_situation = string_to_matrix(data["map_situation"].display())
-            your_turn_sentence = data["your_turn_sentence"]
-            player = data["player"]
-
-            print(game_map.display())
-            possible_moves, player_pos = check_possible_moves(player, map_situation)
-            choice = input(your_turn_sentence)
-            move_player(choice, possible_moves, player_pos, game_map)
-            print(game_map.display())
-
 
 def main():
     # Terminal Template
