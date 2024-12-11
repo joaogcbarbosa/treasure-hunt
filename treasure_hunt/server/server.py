81af3a06204e0f67463e9c9ca7404bc8851fda10
joaogcbarbosa <joaogcb29@gmail.com>
2024-12-10 22:25:28 -0300

    Refactor: all client connections

--- treasure_hunt/server/server.py
+++ treasure_hunt/server/server.py
@@ -15,17 +15,20 @@ def server_runner(number_of_players: int) -> None:
         print("Running server...")
         print("Waiting for players to connect")
         print()
+        
         accepted_connections = 0
+        connections: list[socket] = []
         coin_db = init_coin_db(number_of_players)
 
         while True:
             if accepted_connections < number_of_players:
                 conn, addr = s.accept()
                 print(f"Connection from {addr} accepted")
+                connections.append(conn)
                 accepted_connections += 1
                 print("Waiting for other players")
             elif accepted_connections == number_of_players:
-                game(number_of_players, coin_db, conn) 
+                game(number_of_players, coin_db, connections) 
             else:
                 print(f"Connection from {addr} refused")
                 conn.close()
