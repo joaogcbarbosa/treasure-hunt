14ca03375bb91dbff918e51a8561dfaed58bc06a
joaogcbarbosa <joaogcb29@gmail.com>
2024-12-14 00:53:09 -0300

    Refactor: removes dead code

--- treasure_hunt/server/game.py
+++ treasure_hunt/server/game.py
@@ -14,29 +14,3 @@ def spot_players(number_of_players: int) -> None:
             height, width = randint(0, height_bound), randint(0, width_bound)
         game_map.update(height, width, player)
         occupied_spots.append((height, width, player))
-
-
-# def game(number_of_players: int, coin_db: dict[str, list], connections: list[socket]):
-#     global rnd
-#     if rnd == 0:
-#         print("Initializing map.\n")
-#         sleep(2)
-#         spot_players(number_of_players, game_map)  # Altera instância original do mapa
-#         rnd += 1
-
-#     for conn in connections:
-#         print(conn)
-#         data = {
-#             "game_map": game_map,
-#             "coin_db": coin_db,
-#         }
-#         data = pickle.dumps(data)
-
-#         map_situation = None
-#         db_situation = None
-#         while not (isinstance(map_situation, GameMap) and isinstance(db_situation, dict)):
-#             conn.sendall(data)  # Envia a instância original do mapa
-#             data_from_client = pickle.loads(conn.recv(2048))
-#             map_situation = data_from_client["game_map"]  # Carrega mapa alterado pelo cliente
-#             db_situation = data_from_client["coin_db"]  # Carrega banco alterado pelo cliente
-#         coin_db.update(db_situation)
