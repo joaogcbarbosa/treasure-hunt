a5b4f9104419d4ad49627792bb150e95d7d9e0bd
joaogcbarbosa <joaogcb29@gmail.com>
2024-12-31 13:53:21 -0300

    Refactor: checking if position is special map position

--- treasure_hunt/server/game.py
+++ treasure_hunt/server/game.py
@@ -1,16 +1,19 @@
-from .game_map import game_map
 from random import randint
+
+from treasure_hunt.models.map import GameMap
+from treasure_hunt.utils.converters import string_to_matrix
 
 rnd = 0
 occupied_spots = []
 
-def spot_players(number_of_players: int) -> None:
+def spot_players(number_of_players: int, game_map: GameMap) -> None:
     global occupied_spots
     for i in range(1, number_of_players + 1):
         height_bound, width_bound = game_map.bounds()
         height, width = randint(0, height_bound), randint(0, width_bound)
         player = f"P{i}"
-        while (height, width) in occupied_spots:
+        map_situation = string_to_matrix(game_map.display())
+        while (height, width) in occupied_spots or map_situation[height][width] == "X":
             height, width = randint(0, height_bound), randint(0, width_bound)
         game_map.update(height, width, player)
         occupied_spots.append((height, width, player))
