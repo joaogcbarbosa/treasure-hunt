2599a442098f46520128cac14a5274009ecdbcac
joaogcbarbosa <joaogcb29@gmail.com>
2024-12-31 19:13:07 -0300

    Refactor: tweak on variable

--- treasure_hunt/server/game.py
+++ treasure_hunt/server/game.py
@@ -3,12 +3,9 @@ from random import randint
 from treasure_hunt.models.map import GameMap
 from treasure_hunt.utils.converters import string_to_matrix
 
-rnd = 0
-occupied_spots = []
-
 
 def spot_players(number_of_players: int, game_map: GameMap) -> None:
-    global occupied_spots
+    occupied_spots = []
     for i in range(1, number_of_players + 1):
         height_bound, width_bound = game_map.bounds()
         height, width = randint(0, height_bound), randint(0, width_bound)
