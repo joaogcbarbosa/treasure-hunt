a49c33b58d670bcf322c6b9ff216ed46ce3cb460
joaogcbarbosa <joaogcb29@gmail.com>
2025-01-04 09:10:27 -0300

    Feat: logger

---
+++ treasure_hunt/utils/logger.py
@@ -0,0 +1,9 @@
+from constants import LOGGER_FILE_PATH
+
+
+def write_log(msg: str) -> None:
+    try:
+        with open(file=LOGGER_FILE_PATH, mode="a") as f:
+            f.write(f"{msg}\n")
+    except Exception:
+        print("Please, fill the variable LOGGER_FILE_PATH on ../utils/constants.py")
