9a02b0a9a3afe0a709cec149d4c7347748a0be86
joaogcbarbosa <joaogcb29@gmail.com>
2025-01-03 22:44:31 -0300

    Fix

--- treasure_hunt/server/game.py
+++ treasure_hunt/server/game.py
@@ -1,17 +1,23 @@
-from random import randint
-
 from treasure_hunt.models.map import GameMap
 from treasure_hunt.utils.converters import string_to_matrix
 
+from random import sample
 
 def spot_players(number_of_players: int, game_map: GameMap) -> None:
-    occupied_spots = []
-    for i in range(1, number_of_players + 1):
-        height_bound, width_bound = game_map.bounds()
-        height, width = randint(0, height_bound), randint(0, width_bound)
+    map_situation = string_to_matrix(game_map.display())
+    all_positions = [
+        (height, width) 
+        for height in range(game_map.bounds()[0]) 
+        for width in range(game_map.bounds()[1])
+        if map_situation[height][width] != "X"  # Filtra posições com "X"
+    ]
+    
+    # Garante que há posições disponíveis suficientes
+    if len(all_positions) < number_of_players:
+        raise ValueError("Não há posições disponíveis suficientes para todos os jogadores.")
+    
+    occupied_positions = sample(all_positions, number_of_players)
+    
+    for i, (height, width) in enumerate(occupied_positions, start=1):
         player = f"P{i}"
-        map_situation = string_to_matrix(game_map.display())
-        while (height, width) in occupied_spots or map_situation[height][width] == "X":
-            height, width = randint(0, height_bound), randint(0, width_bound)
         game_map.update(height, width, player)
-        occupied_spots.append((height, width, player))
