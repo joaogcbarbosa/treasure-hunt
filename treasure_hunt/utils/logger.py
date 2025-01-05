8cacd3193e2336e00d1482dc661f3e364dbfe318
joaogcbarbosa <joaogcb29@gmail.com>
2025-01-05 00:00:50 -0300

    Refactor: tweak

--- treasure_hunt/utils/logger.py
+++ treasure_hunt/utils/logger.py
@@ -5,5 +5,5 @@ def write_log(msg: str) -> None:
     try:
         with open(file=LOGGER_FILE_PATH, mode="a") as f:
             f.write(f"{msg}\n")
-    except Exception:
-        print("Please, fill the variable LOGGER_FILE_PATH on ../utils/constants.py")
+    except Exception as e:
+        print(e)
